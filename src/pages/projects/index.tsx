12b107094d64d6d0b7689265cb4a3424f8964b2f
haekal yudhistira <haekal@paywake.com>
2023-12-24 22:12:17 +0700

    fix: header gancet due to padding and flex

--- src/pages/projects/index.tsx
+++ src/pages/projects/index.tsx
@@ -28,42 +28,47 @@ function ProjectsPage() {
   };
 
   return (
-    <div className="bg-gray-300 h-full">
-      <div className="flex flex-wrap justify-center">
-        {COMPANY_HISTORIES.map((ch, idx) => (
-          <div key={idx} className="p-10">
-            <div className="w-80">
-              <div className="flex flex-wrap justify-between">
-                <p className="font-bebas">{ch.companyName}</p>{" "}
-                <button
-                  className="text-xs underline"
-                  onClick={() => toogleHide(idx)}
-                >
-                  {!ch.isShownProject ? "detail" : "hide"}
-                </button>
+    // full height of the screen
+    <div className="bg-gray-300 h-full pt-5">
+      {/* fix height & overflow, keeps the footer visible */}
+      <div className="h-85 overflow-auto">
+        <div className="flex flex-wrap justify-center">
+          {COMPANY_HISTORIES.map((ch, idx) => (
+            // p-10 would cramp & push the box causing overflow
+            <div key={idx} className="py-5">
+              <div className="w-80">
+                <div className="flex flex-wrap justify-between">
+                  <p className="font-bebas">{ch.companyName}</p>{" "}
+                  <button
+                    className="text-xs underline"
+                    onClick={() => toogleHide(idx)}
+                  >
+                    {!ch.isShownProject ? "detail" : "hide"}
+                  </button>
+                </div>
+                <div>
+                  <p className="text-xs">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil
+                    dolores illo accusantium quod veritatis.
+                  </p>
+                </div>
               </div>
-              <div>
-                <p className="text-xs">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil
-                  dolores illo accusantium quod veritatis.
-                </p>
+              {ch.isShownProject && (
+                <Modal onClose={closeModal}>{renderProjects(ch.projects)}</Modal>
+              )}
+              <div className="w-full">
+                <p className="font-bebas">tech stacks:</p>
+                <div className="border border-black  flex flex-wrap w-80 justify-center">
+                  {ch.tackStacks.map((ts, idx) => (
+                    <div className="p-1 font-semibold text-sm" key={idx}>
+                      {ts}
+                    </div>
+                  ))}
+                </div>
               </div>
             </div>
-            {ch.isShownProject && (
-              <Modal onClose={closeModal}>{renderProjects(ch.projects)}</Modal>
-            )}
-            <div className="w-full">
-              <p className="font-bebas">tech stacks:</p>
-              <div className="border border-black  flex flex-wrap w-80 justify-center">
-                {ch.tackStacks.map((ts, idx) => (
-                  <div className="p-1 font-semibold text-sm" key={idx}>
-                    {ts}
-                  </div>
-                ))}
-              </div>
-            </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
     </div>
   );
